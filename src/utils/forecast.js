1d92138d54494aa66bb0b738f4ed6752f90311a8
unknown <dcomonicar@gmail.com>
2020-07-01 22:14:41 +0800

    Updated forecast to include new data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const  forecast = (latitude, longitude, callback) => {
                 callback('Unable to find location', undefined)
             } else {
                 callback(undefined, 
-                    body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' out.'
+                    body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' out. The humidity is '+body.current.humidity +'.'
                 //     weather_description: response.body.current.weather_descriptions[0],
                 //     temperature: response.body.current.temperature,
                 //     feels_like: response.body.current.feelslike             
